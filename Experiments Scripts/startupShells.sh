66fbf3fc1073873caa23f3c8890770e7691773d6
Murali <muralis@cse.iitb.ac.in>
2015-01-28 15:13:25 +0530

    README UPDATE and startupShells update

--- Experiments Scripts/startupShells.sh
+++ Experiments Scripts/startupShells.sh
@@ -2,7 +2,8 @@
 
 TERM='urxvt'
 #easy shortcuts
-$TERM -g +0+0 -e ssh webq@10.129.41.8 &    #gen
+#$TERM -g +0+0 -e ssh webq@10.129.41.8 &    #gen
+$TERM -g +0+540 -e ssh webq@10.129.26.130 &     #gen
 $TERM -g +1000+0 -e ssh webq@10.129.41.67 &    #check
 $TERM -g +1000+540 -e ssh webq@10.129.49.76 &    #server
 $TERM -g +0+540 -e ssh webq@10.129.2.55 &     #laod gen
