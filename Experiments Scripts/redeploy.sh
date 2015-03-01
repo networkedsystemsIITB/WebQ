b1bea3c3a2d9f95c108c3348e23a60eb960c4991
stanlyjohn2 <stanlyjohn2@gmail.com>
2015-03-01 17:04:20 +0530

    Modified Redeploy to use root

--- Experiments Scripts/redeploy.sh
+++ Experiments Scripts/redeploy.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-#run from apache server(same as capacity estimator)
-
+#run from proxy1 server(same as capacity estimator)
 tokencheck="10.129.41.67"
 tokengen="10.129.26.130"
 server="10.129.49.76"
+vachaspati="10.129.2.55"
 server2="comp4"
 
 if [ "$#" -ne 1 ];
@@ -17,28 +17,26 @@ fi
 #kill all the components
 echo "Killing all components";
 killall java;
-ps ax | grep CapacityManager | sed 's|  | |g' | cut -d' ' -f2 | xargs -I {} sudo kill -9 {}
+# ps ax | grep CapacityManager | sed 's|  | |g' | cut -d' ' -f2 | xargs -I {} sudo kill -9 {}
 ssh root@$tokencheck "killall lighttpd &> /dev/null";
 sudo service apache2 stop;
 
 #stop server, make the proxy1 code, and copy it in /usr/lib/cgi-bin
 #then start the server
-echo "Remaking the proxy1";
-cd ../TokenGenNew;
-make;
+# echo "Remaking the proxy1";
+# cd ../TokenGenNew;
+# make;
 
 #cleaning up all the log files
-echo "Cleaning up the log files"
-sudo rm /home/webq/TokenGenNew/proxy1.log &> /dev/null
-rm /home/webq/CapacityEstimator/javapersecond.log &> /dev/null
-rm /home/webq/CapacityEstimator/javadebug.log &>/dev/null
-ssh webq@vachaspati rm /home/webq/summary60.csv &> /dev/null
-ssh webq@vachaspati rm /home/webq/summary110.csv &> /dev/null
-#ssh bhavin@pollaczek rm /home/bhavin/summary.csv &> /dev/null
-#ssh bhavin@pollaczek rm /home/bhavin/summary.csv &> /dev/null
+# echo "Cleaning up the log files"
+# sudo rm /home/webq/webq-repo/TokenGenNew/proxy1.log &> /dev/null
+# rm /home/webq/webq-repo/CapacityEstimator/javapersecond.log &> /dev/null
+# rm /home/webq/webq-repo/CapacityEstimator/javadebug.log &>/dev/null
+# ssh webq@$vachaspati rm /home/webq/summary60.csv &> /dev/null
+# ssh webq@$vachaspati rm /home/webq/summary110.csv &> /dev/null
 
 #start the apache server
-sudo service apache2 start;
+sudo service apache2 start
 
 #hit the URL once
 echo "Hitting the URL once";
@@ -54,14 +52,13 @@ bash run.sh;
 echo "Starting the lighttpd server";
 ssh root@$tokencheck "bash /home/webq/tokencheck/"$tokencheckscript;
 
-echo "Setting $server and $server2 governor to performance:"
-for i in `seq 0 3`; do
-	ssh root@$server "cpufreq-set -g performance -c $i";
-	ssh root@$server2 "cpufreq-set -g performance -c $i";
-done;
-echo "Checking if governor is set correctly:"
-ssh root@$server "cpufreq-info | grep \"governor \"";
-ssh root@$server2 "cpufreq-info | grep \"governor \"";
+# echo "Setting $server and $server2 governor to performance:"
+# for i in `seq 0 3`; do
+# 	ssh root@$server "cpufreq-set -g performance -c $i";
+# 	ssh root@$server2 "cpufreq-set -g performance -c $i";
+# done;
+# echo "Checking if governor is set correctly:"
+# ssh root@$server "cpufreq-info | grep \"governor \"";
+# ssh root@$server2 "cpufreq-info | grep \"governor \"";
 
 echo "################# REDEPLOYMENT ATTEMPT FINISHED ##################";
-
