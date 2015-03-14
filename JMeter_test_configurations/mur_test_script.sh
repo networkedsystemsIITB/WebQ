07426c55a33b5b9064cea90d714e51ab6df72119
stanlyjohn2 <stanlyjohn2@gmail.com>
2015-03-14 11:01:10 +0530

    goodput and reponse time jmx and script

--- JMeter_test_configurations/mur_test_script.sh
+++ JMeter_test_configurations/mur_test_script.sh
@@ -1,5 +1,8 @@
 
 
+for threadno in {1..50}
+do
+    sed -i 's/threads">\([0-9]\+\)/threads">'$threadno'/' murali_tstMarch09.jmx
+    sed -i 's/14-\([0-9]\+\)/14-'$threadno'/' murali_tstMarch09.jmx
+done
 
-sed -i 's/threads">\([0-9]\+\)/threads">NUMBER/' murali_tstMarch09.jmx
-sed -i 's/14-\([0-9]\+\)/14-NUMBER/' murali_tstMarch09.jmx
