b7fddb2546616f318ca640b678644d03968c4314
Murali S <muralisc@gmail.com>
2015-03-25 16:37:04 +0530

    added auto wakeup calls for machines for Waking on Lan

--- Experiments Scripts/startupShells.sh
+++ Experiments Scripts/startupShells.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+
+curl 'cuse.iitb.ac.in/wol/wol.php?mac=00:22:4d:b2:12:7b'        #49.76 server
+curl 'cuse.iitb.ac.in/wol/wol.php?mac=00:22:4d:b1:fc:a9'        #41.67 check
+curl 'cuse.iitb.ac.in/wol/wol.php?mac=7c:05:07:10:41:2f'        #26.130 gen
+
 TERM='urxvt'
 #easy shortcuts
 #$TERM -g +0+0 -e ssh webq@10.129.41.8 &    #gen
