84ef79b9a177a07bed06c7161ccb1564dd6536c3
Murali S <muralis@cse.iitb.ac.in>
2015-03-30 22:17:38 +0530

    changed scripts to use nohup so that remote invocation is possible

--- CapacityEstimator/run.sh
+++ CapacityEstimator/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nohup java -cp jars/commons-math3-3.3.jar:jars/log4j-1.2.14.jar:bin com.webq.capest.CapacityManager &
+nohup java -cp jars/commons-math3-3.3.jar:jars/log4j-1.2.14.jar:bin com.webq.capest.CapacityManager > foo.out 2> foo.err < /dev/null &
