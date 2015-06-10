12ce17b351e2f8c66388aa58dfb382408b18209b
Murali Suresh <muralisc@gmail.com>
2015-06-10 19:53:23 +0530

    use machine

--- bin/dev_setup.sh
+++ bin/dev_setup.sh
@@ -2,20 +2,28 @@
 
 if [ -z "$1" ];
 then
-    echo "pass user account. eg ~/dev_setup <username>"
+    echo "pass user account. eg ~/dev_setup <username> <systemname>"
+    exit
+fi
+if [ -z "$2" ];
+then
+    echo "pass user account. eg ~/dev_setup <username> <systemname>"
     exit
 fi
 
 echo 'alias jl="vim ~/webq/journal/`date --iso`"' >> ~/.aliases.sh
 
-sudo cat << EOF >> /etc/hosts
-10.129.49.76	server
-10.129.41.67	check
-10.129.26.130   gen
-10.129.2.55 vacha
+cat << EOF >> ~/.aliases.sh
+export PS1="$PS1 $2
+"
 EOF
 
-cat << EOF >> .ssh/config
+echo "10.129.49.76    server" | sudo tee -a /etc/hosts
+echo "10.129.41.67    check" | sudo tee -a /etc/hosts
+echo "10.129.26.130   gen" | sudo tee -a /etc/hosts
+echo "10.129.2.55 vacha" | sudo tee -a /etc/hosts
+
+cat << EOF >> ~/.ssh/config
 Host server
 	HostName 10.129.49.76
 	User	$1
