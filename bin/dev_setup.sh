5368ccb40ffdfee5eccd5ca10b3517567a427f77
murali suresh <muralis@cse.iitb.ac.in>
2015-06-17 12:04:06 +0530

    dev script update

--- bin/dev_setup.sh
+++ bin/dev_setup.sh
@@ -5,18 +5,13 @@ then
     echo "pass user account. eg ~/dev_setup <username>"
     exit
 fi
-if [ -z "$2" ];
-then
-    echo "pass user account. eg ~/dev_setup <username>"
-    exit
-fi
-
 
 echo 'alias jl="vim ~/webq/journal/`date --iso`"' >> ~/.localaliases.sh
 
 echo "10.129.49.76    server" | sudo tee -a /etc/hosts
 echo "10.129.41.67    check" | sudo tee -a /etc/hosts
 echo "10.129.26.130   gen" | sudo tee -a /etc/hosts
+echo "10.129.41.17   gen2" | sudo tee -a /etc/hosts
 echo "10.129.2.55 vacha" | sudo tee -a /etc/hosts
 
 cat << EOF >> ~/.ssh/config
@@ -32,11 +27,16 @@ Host gen
 	HostName 10.129.26.130
 	User	$1
 
+Host gen2
+	HostName 10.129.41.17
+	User	$1
+
 Host vacha
 	HostName 10.129.2.55
 	User	$1
 EOF
 
+cd ~/webq
 git update-index --assume-unchanged TokenCheck/lighttpd/src/*.o
 git update-index --assume-unchanged TokenCheck/lighttpd/src/.libs/**
 git update-index --assume-unchanged TokenCheck/lighttpd/**/Makefile
