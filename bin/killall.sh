fcf5a18f0ae8e750a07631404c6303ea04c87753
murali suresh <muralis@cse.iitb.ac.in>
2015-06-29 19:15:13 +0530

    new time format for javapersecond log

--- bin/killall.sh
+++ bin/killall.sh
@@ -5,7 +5,12 @@ source ips.sh
 
 #{{{ kill all components
 printf " %d\n%s%43s\n" $? $marker "Killing all components" | tee -a $log_file
-for machine in $tokengen1 $tokengen2
+if [ -z "$gens" ];
+then
+    gens="$tokengen1 $tokengen2"
+fi
+echo "in kilall gens: $gens"
+for machine in $gens
 do
     ssh root@$machine "killall java"
     ssh root@$machine "killall apache2"
