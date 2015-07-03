9e4c3d819b14e69d8ca827e679a4fd6fb418178b
murali suresh <muralis@cse.iitb.ac.in>
2015-07-03 15:13:29 +0530

    Redeploy related changes

--- bin/killall.sh
+++ bin/killall.sh
@@ -2,6 +2,7 @@
 
 cd ~/webq/bin
 source ips.sh
+printf "`date`" | tee -a $log_file
 
 #{{{ kill all components
 printf " %d\n%s%43s\n" $? $marker "Killing all components" | tee -a $log_file
@@ -9,11 +10,12 @@ if [ -z "$gens" ];
 then
     gens="$tokengen1 $tokengen2"
 fi
-echo "in kilall gens: $gens"
+echo "in kilall, gens considered are: $gens"
 for machine in $gens
 do
-    ssh root@$machine "killall java"
-    ssh root@$machine "killall apache2"
+    # redirect stderr to stdout so that we can log it 
+    ssh root@$machine "killall java" 2>&1 | tee -a $log_file > /dev/null
+    ssh root@$machine "killall apache2" 2>&1 | tee -a $log_file > /dev/null
 done
-ssh root@$tokencheck "killall lighttpd";
+ssh root@$tokencheck "killall lighttpd" 2>&1  | tee -a $log_file > /dev/null
 #}}}
