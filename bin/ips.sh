a7787aeb66949c2546108ed6845258c615d8f18e
murali suresh <muralis@cse.iitb.ac.in>
2015-07-15 20:03:00 +0530

    adding ravis ip

--- bin/ips.sh
+++ bin/ips.sh
@@ -8,15 +8,19 @@ log_file=large_log.log
 declare -A tokencheck
 tokencheck[stanly]="10.129.41.17"
 tokencheck[murali]="10.129.41.67"
+tokencheck[ravi]="10.129.26.133"
 
 declare -A tokengen1
 tokengen1[stanly]="10.129.26.133"
 tokengen1[murali]="10.129.26.133"
+tokengen1[ravi]="10.129.41.67"
 
 declare -A capacityEstimator
 capacityEstimator[stanly]="10.129.26.133"
 capacityEstimator[murali]="10.129.41.17"
+capacityEstimator[ravi]="10.129.41.67"
 
 declare -A tokengen2
 tokengen2[stanly]=""
 tokengen2[murali]="10.129.41.17"
+tokengen2[ravi]=""
