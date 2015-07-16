2c86cc96d2f9b9a371f91d235f180a5fd51e3ec2
murali suresh <muralis@cse.iitb.ac.in>
2015-07-16 13:28:06 +0530

    change lighttpd port to 9000

--- TokenGenNew/proxy1.c
+++ TokenGenNew/proxy1.c
@@ -365,7 +365,7 @@ void main(void) {/*{{{*/
             char* env_var = getenv("QUERY_STRING");
             char* request_limit = strchr(env_var, '=') + 1;
             char url_to_visit[100];
-            strcpy(url_to_visit, "http://10.129.41.67:8000/test.php?limit=");
+            strcpy(url_to_visit, "http://10.129.41.67:9000/test.php?limit=");
             strcat(url_to_visit, (const char*) request_limit);
 
             printf("Refresh: %d; url=%s&hash=%s&token=%s\n", time_to_wait,
