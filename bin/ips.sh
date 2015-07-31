93efd8702f04b325fb98ea6930af97be537189d5
murali suresh <muralis@cse.iitb.ac.in>
2015-07-31 22:13:36 +0530

    new server, gen

--- bin/ips.sh
+++ bin/ips.sh
@@ -12,7 +12,7 @@ tokencheck[ravi]="10.129.26.133"
 
 declare -A tokengen1
 tokengen1[stanly]="10.129.26.133"
-tokengen1[murali]="10.129.26.133"
+tokengen1[murali]="10.129.28.32"
 tokengen1[ravi]="10.129.41.67"
 
 declare -A capacityEstimator
@@ -24,3 +24,13 @@ declare -A tokengen2
 tokengen2[stanly]=""
 tokengen2[murali]="10.129.41.17"
 tokengen2[ravi]=""
+
+#                          |murali   |  stan      ravi
+#--------------------------|---------|------------------
+#      | 10.129.41.17      | gen2    |  check
+#      | 10.129.41.67      | check   |            gen1
+#      | 10.129.26.133     |         |  gen1      check
+#      | 10.129.2.55       | vacha   |  vacha     vacha
+#      | 10.129.49.76      |         |  server
+# stan | 10.129.28.32      | gen1    |
+# mura | 10.129.28.146     | server  |
