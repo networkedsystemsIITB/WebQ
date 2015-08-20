6295991e68793aae3c6900e52ab6a312cd68385f
Murali Suresh <muralisc@gmail.com>
2015-08-20 20:38:13 +0530

    better makeknown hosts

--- bin/makeKnownHost.sh
+++ bin/makeKnownHost.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # use this script to enable passwordless entry into another account
-# like to setup passwordless entry into root account of all the comps 
+# like to setup passwordless entry into root account of all the comps
 # ./makeKnownHost.sh root <ip>
 
 # echo "dev setup should be run before runnign this file"
 
 if [ -z "$1" -o  -z "$2" ];
 then
-    echo "input username ./redeploy.sh <username> <hostnam/ip>"
+    echo "input username ./makeKnownHost.sh <username> <hostnam/ip>"
     exit
 else
     USER=$1
@@ -19,6 +19,9 @@ then
     ssh-keygen -t rsa
 fi
 
+ssh -oNumberOfPasswordPrompts=0  ${USER}@${IP} 'ls'
+if [ $? -ne 0 ]; then
+    ssh ${USER}@${IP} 'mkdir -p ~/.ssh'
+    cat ~/.ssh/id_rsa.pub |ssh ${USER}@${IP} 'cat >> .ssh/authorized_keys'
+fi
 
-ssh ${USER}@${IP} 'mkdir -p ~/.ssh'
-cat ~/.ssh/id_rsa.pub |ssh ${USER}@${IP} 'cat >> .ssh/authorized_keys'
