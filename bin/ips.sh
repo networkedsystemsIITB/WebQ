a8f92850264f152bc18e81ac6b973310e087139c
Murali Suresh <muralisc@gmail.com>
2015-08-28 23:08:04 +0530

    update ips

--- bin/ips.sh
+++ bin/ips.sh
@@ -25,12 +25,12 @@ tokengen2[stanly]=""
 tokengen2[murali]="10.129.28.160"
 tokengen2[ravi]=""
 
-#                          |murali   |  stan      ravi
-#--------------------------|---------|------------------
-#      | 10.129.28.160      | gen2    |  check
-#      | 10.129.41.67      | check   |            gen1
-#      | 10.129.26.133     |         |  gen1      check
-#      | 10.129.2.55       | vacha   |  vacha     vacha
+#                          |murali   |  matlab
+#--------------------------|---------|---------
+#      | 10.129.28.160     | gen2    |
+#      | 10.129.41.67      | check   |
+#      | 10.129.26.133     | sapre1  |  gen1
+#      | 10.129.2.55       | vacha   |  vacha
 #      | 10.129.49.76      |         |  server
-# stan | 10.129.28.32      | gen1    |
+# stan | 10.129.28.32      | gen1    |  check
 # mura | 10.129.28.146     | server  |
