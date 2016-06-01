a183195f791e5d1452b8c4eddfe04bf723d5a389
Murali Suresh <muralisc@gmail.com>
2016-06-02 03:06:26 +0530

    machine allocations

--- bin/ips.sh
+++ bin/ips.sh
@@ -25,12 +25,12 @@ tokengen2[stanly]=""
 tokengen2[murali]="10.129.28.160"
 tokengen2[ravi]=""
 
-#                          |murali 
-#--------------------------|--------
-#      | 10.129.28.160     | gen2  
-#      | 10.129.41.67      | not in n/w(check)
-#      | 10.129.26.133     | sapre1
-#      | 10.129.2.55       | vacha 
-#      | 10.129.49.76      | server( spare2 )
-# stan | 10.129.28.32      | gen1  
-# mura | 10.129.28.146     | server
+#                           |               | murali             | srishti
+#-------------------------- | --------      | ------------------ | ----------
+#                           | 10.129.28.160 | gen2&ce            | check
+#                           | 10.129.41.67  | check              | gen1&ce
+#                           | 10.129.26.133 | sapre1(gen3)       |
+#                           | 10.129.49.76  | server( spare2 )   | server
+# vachaspati                | 10.129.2.55   | vacha              | vacha
+# stan mtp                  | 10.129.28.32  | gen1               |
+# muru mtp                  | 10.129.28.146 | server             | 
