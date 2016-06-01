2c6744572bb7d7c383d341036defa26894bdea61
murali suresh <muralis@cse.iitb.ac.in>
2016-06-02 02:56:34 +0530

    scripts for server

---
+++ Server Scripts/req1.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$start=microtime(TRUE);
+$servername = "localhost";
+$username = "root";
+$password = "webqwebq";
+$dbname = "moodleM";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+         die("Connection failed: " . $conn->connect_error);
+}
+
+for ($x = 0; $x <= 500; $x++) {
+$sql = "select id,category,sortorder from mdl_course";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+         // output data of each row
+         while($row = $result->fetch_assoc()) {
+                   //   echo "<br> id: ". $row["id"]. " - Name: ". $row["category"]. " " . $row["sortorder"] . "<br>";
+                           }
+} else {
+         echo "0 results";
+}
+}
+$conn->close();
+$end=microtime(TRUE);
+echo "<h1>".$limit."\n".($end-$start)." Seconds</h1>";
+?> 
+
+</body>
+</html>
