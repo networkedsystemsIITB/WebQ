058936e542598d2ff6395e168a0d33023c163e69
Murali Suresh <muralisc@gmail.com>
2016-06-03 19:39:14 +0530

    remove class files from dev setup as they are removed in prev commit

--- bin/dev_setup.sh
+++ bin/dev_setup.sh
@@ -21,19 +21,3 @@ git update-index --assume-unchanged TokenCheck/lighttpd/src/spawn-fcgi
 git update-index --assume-unchanged TokenGenNew/waittimedebug.log
 git update-index --assume-unchanged TokenGenNew/proxy1
 git update-index --assume-unchanged TokenGenNew/proxy1.log
-
-# no need to commit class files every time
-git update-index --assume-unchanged CapacityEstimator/bin/com/webq/capest/CapacityChangeDetector.class
-git update-index --assume-unchanged CapacityEstimator/bin/com/webq/capest/CapacityEstimator\$cProgressionMethod.class
-git update-index --assume-unchanged CapacityEstimator/bin/com/webq/capest/CapacityEstimator.class
-git update-index --assume-unchanged CapacityEstimator/bin/com/webq/capest/CapacityManager\$1.class
-git update-index --assume-unchanged CapacityEstimator/bin/com/webq/capest/CapacityManager\$2.class
-git update-index --assume-unchanged CapacityEstimator/bin/com/webq/capest/CapacityManager\$3.class
-git update-index --assume-unchanged CapacityEstimator/bin/com/webq/capest/CapacityManager\$4.class
-git update-index --assume-unchanged CapacityEstimator/bin/com/webq/capest/CapacityManager.class
-git update-index --assume-unchanged CapacityEstimator/bin/com/webq/capest/PerSecondLogging.class
-git update-index --assume-unchanged CapacityEstimator/bin/com/webq/capest/PowerRatio.class
-git update-index --assume-unchanged CapacityEstimator/bin/com/webq/capest/PowerRatioData.class
-git update-index --assume-unchanged CapacityEstimator/bin/com/webq/capest/PropertiesFile.class
-git update-index --assume-unchanged CapacityEstimator/bin/com/webq/capest/TokenCheckCommunicator.class
-git update-index --assume-unchanged CapacityEstimator/bin/com/webq/capest/TokenGenCommunicator.class
